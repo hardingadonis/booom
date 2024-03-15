0b772cd9f4f6a3bb98764927177924b805742a45
Minh Vương <hardingadonis@gmail.com>
2024-03-15 17:00:46 +0700

    add effect when render player and tower

--- src/scenes/PlayScene.cpp
+++ src/scenes/PlayScene.cpp
@@ -19,6 +19,14 @@ void PlayScene::Update(float delta)
 
 void PlayScene::Render(SDL_Renderer* renderer)
 {
-	this->m_tower->Render(renderer);
-	this->m_player->Render(renderer);
+	if (this->m_tower->GetOrigin().y + 100 < this->m_player->GetOrigin().y)
+	{
+		this->m_tower->Render(renderer);
+		this->m_player->Render(renderer);
+	}
+	else
+	{
+		this->m_player->Render(renderer);
+		this->m_tower->Render(renderer);
+	}
 }
